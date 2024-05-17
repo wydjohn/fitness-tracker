828003fc53db1cbcf55dd2423e6e2345e0534cb4
wydjohn <161156445+wydjohn@users.noreply.github.com>
2024-05-17 04:33:02 -0400

    cleaning

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from flask import Flask, request, jsonify, make_response
-from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime, timedelta
 import jwt
-import datetime
+from flask import Flask, jsonify, make_response, request
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import check_password_hash, generate_password_hash
 from functools import wraps
 import os
 import uuid
@@ -15,6 +15,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     public_id = db.Column(db.String(50), unique=True)
@@ -22,58 +23,52 @@ class User(db.Model):
     password = db.Column(db.String(80))
     admin = db.Column(db.Boolean)
 
+
 class WorkoutLog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
     description = db.Column(db.Text)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
 
 def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         token = request.headers.get('x-access-token')
-
         if not token:
             return jsonify({'message': 'Token is missing!'}), 401
-
         try:
             data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
             current_user = User.query.filter_by(public_id=data['public_id']).first()
         except:
             return jsonify({'message': 'Token is invalid!'}), 401
-
         return f(current_user, *args, **kwargs)
     return decorated
+
 
 @app.route('/user', methods=['POST'])
 def create_user():
     data = request.get_json()
     hashed_password = generate_password_hash(data['password'], method='sha256')
-
     new_user = User(public_id=str(uuid.uuid4()), name=data['name'], password=hashed_password, admin=False)
     db.session.add(new_user)
     db.session.commit()
-
     return jsonify({'message': 'New user created!'})
+
 
 @app.route('/login', methods=['POST'])
 def login_user():
     auth = request.authorization
-
     if not auth or not auth.username or not auth.password:
         return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
-
     user = User.query.filter_by(name=auth.username).first()
-
     if not user:
         return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
-
     if check_password_hash(user.password, auth.password):
-        token = jwt.encode({'public_id': user.public_id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'], algorithm="HS256")
-
+        token = jwt.encode({'public_id': user.public_id, 'exp': datetime.utcnow() + timedelta(minutes=30)}, app.config['SECRET_KEY'], algorithm="HS256")
         return jsonify({'token': token})
-
     return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
+
 
 @app.route('/workout', methods=['POST'])
 @token_required
@@ -82,27 +77,27 @@ def add_workout(current_user):
     new_workout = WorkoutLog(description=data['description'], user_id=current_user.id)
     db.session.add(new_workout)
     db.session.commit()
-
     return jsonify({'message': 'Workout log created!'})
+
 
 @app.route('/workout/<user_id>', methods=['GET'])
 @token_required
 def get_workouts(current_user, user_id):
     if not current_user.admin:
         return jsonify({'message': 'Cannot perform that function!'})
-
     workouts = WorkoutLog.query.filter_by(user_id=user_id).all()
     output = [
-        {'id': workout.id, 'date': workout.date, 'description': workout.description, 'user_id': workout.user_id}
+        {'id': workout.id, 'date': workout.date.strftime('%Y-%m-%d %H:%M'), 'description': workout.description, 'user_id': workout.user_id}
         for workout in workouts
     ]
-
     return jsonify({'workouts': output})
+
 
 @app.route('/workoutplan', methods=['POST'])
 @token_required
 def generate_workout_plan(current_user):
     return jsonify({'message': 'Workout plan generated for the user.'})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
