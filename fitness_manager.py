c89cbe9e3d5376f3f2c5e670166ed74c6ca23b90
wydjohn <161156445+wydjohn@users.noreply.github.com>
2024-06-05 14:12:49 -0400

    change variable names

--- fitness_manager.py
+++ fitness_manager.py
@@ -7,83 +7,83 @@ from sklearn.ensemble import RandomForestClassifier
 
 load_dotenv()
 
-DATABASE_URL = os.getenv('DATABASE_URL')
-conn = sqlite3.connect(DATABASE_URL)
-cur = conn.cursor()
+DATABASE_PATH = os.getenv('DATABASE_URL')
+connection = sqlite3.connect(DATABASE_PATH)
+cursor = connection.cursor()
 
-cur.execute('''CREATE TABLE IF NOT EXISTS workout_logs
+cursor.execute('''CREATE TABLE IF NOT EXISTS workout_logs
                (id INTEGER PRIMARY KEY, date TEXT, activity TEXT, duration INTEGER, intensity TEXT)''')
-conn.commit()
+connection.commit()
 
-class WorkoutLogs:
+class WorkoutLogger:
     @staticmethod
-    def create_log(date, activity, duration, intensity):
-        cur.execute("INSERT INTO workout_logs (date, activity, duration, intensity) VALUES (?, ?, ?, ?)",
+    def add_workout_log(date, activity, duration, intensity):
+        cursor.execute("INSERT INTO workout_logs (date, activity, duration, intensity) VALUES (?, ?, ?, ?)",
                     (date, activity, duration, intensity))
-        conn.commit()
+        connection.commit()
     
     @staticmethod
-    def read_logs():
-        cur.execute("SELECT * FROM workout_logs")
-        return cur.fetchall()
+    def fetch_all_logs():
+        cursor.execute("SELECT * FROM workout_logs")
+        return cursor.fetchall()
     
     @staticmethod
-    def update_log(log_id, date=None, activity=None, duration=None, intensity=None):
-        query = "UPDATE workout_logs SET "
-        params = []
+    def modify_workout_log(log_id, date=None, activity=None, duration=None, intensity=None):
+        update_query = "UPDATE workout_logs SET "
+        parameters = []
         if date:
-            query += "date = ?, "
-            params.append(date)
+            update_query += "date = ?, "
+            parameters.append(date)
         if activity:
-            query += "activity = ?, "
-            params.append(activity)
+            update_query += "activity = ?, "
+            parameters.append(activity)
         if duration:
-            query += "duration = ?, "
-            params.append(duration)
+            update_query += "duration = ?, "
+            parameters.append(duration)
         if intensity:
-            query += "intensity = ? "
-            params.append(intensity)
-        query = query.strip(", ")
-        query += " WHERE id = ?"
-        params.append(log_id)
-        cur.execute(query, tuple(params))
-        conn.commit()
+            update_query += "intensity = ? "
+            parameters.append(intensity)
+        update_query = update_query.rstrip(", ")
+        update_query += " WHERE id = ?"
+        parameters.append(log_id)
+        cursor.execute(update_query, tuple(parameters))
+        connection.commit()
     
     @staticmethod
-    def delete_log(log_id):
-        cur.execute("DELETE FROM workout_logs WHERE id = ?", (log_id,))
-        conn.commit()
+    def remove_workout_log(log_id):
+        cursor.execute("DELETE FROM workout_logs WHERE id = ?", (log_id,))
+        connection.commit()
 
-class WorkoutAnalysis:
+class WorkoutPredictor:
     @staticmethod
-    def analyze_patterns():
-        df = pd.read_sql_query("SELECT * FROM workout_logs", conn)
-        print(df.describe())
+    def display_workout_data_summary():
+        dataframe = pd.read_sql_query("SELECT * FROM workout_logs", connection)
+        print(dataframe.describe())
     
     @staticmethod
-    def suggest_workout_plan(user_data):
-        df = pd.read_sql_query("SELECT * FROM workout_logs", conn)
-        if len(df) < 10:  
-            return "Insufficient data for personalized workout suggestions."
+    def recommend_workout_plan(input_features):
+        dataframe = pd.read_sql_query("SELECT * FROM workout_logs", connection)
+        if len(dataframe) < 10:  
+            return "Insufficient data for personalized workout recommendations."
         
-        df = pd.get_dummies(df, columns=['intensity'])  
-        X = df.drop(['id', 'date', 'activity'], axis=1)
-        y = df['activity']
+        dataframe = pd.get_dummies(dataframe, columns=['intensity'])  
+        features = dataframe.drop(['id', 'date', 'activity'], axis=1)
+        target = dataframe['activity']
         
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        features_train, features_test, target_train, target_test = train_test_split(features, target, test_size=0.2, random_state=42)
         
-        clf = RandomForestClassifier(n_estimators=100)
-        clf.fit(X_train, y_train)
+        classifier = RandomForestClassifier(n_estimators=100)
+        classifier.fit(features_train, target_train)
         
-        suggested_activity = clf.predict([user_data])
-        return suggested_activity
+        recommended_activity = classifier.predict([input_features])
+        return recommended_activity
 
 if __name__ == "__main__":
-    WorkoutLogs.create_log('2023-07-01', 'Running', 30, 'High')
-    print("Log Created")
-    print("Current Logs:", WorkoutLogs.read_logs())
-    WorkoutLogs.update_log(1, activity='Cycling')
-    print("After Update:", WorkoutLogs.read_logs())
-    WorkoutAnalysis.analyze_patterns()
-    user_data = [20, 0, 1, 0]  
-    print("Suggested Activity:", WorkoutAnalysis.suggest_workout_plan(user_data))
+    WorkoutLogger.add_workout_log('2023-07-01', 'Running', 30, 'High')
+    print("Workout Log Added")
+    print("All Workout Logs:", WorkoutLogger.fetch_all_logs())
+    WorkoutLogger.modify_workout_log(1, activity='Cycling')
+    print("Logs After Update:", WorkoutLogger.fetch_all_logs())
+    WorkoutPredictor.display_workout_data_summary()
+    input_for_prediction = [20, 0, 1, 0]  
+    print("Recommended Workout Activity:", WorkoutPredictor.recommend_workout_plan(input_for_cameras))
