7b57f428b6cac11f1f0e78dfe8720c187280d8cf
wydjohn <161156445+wydjohn@users.noreply.github.com>
2024-06-12 06:10:46 -0400

    optimize the naming

--- fitness_manager.py
+++ fitness_manager.py
@@ -7,91 +7,91 @@ from sklearn.ensemble import RandomForestClassifier
 
 load_dotenv()
 
-DATABASE_PATH = os.getenv('DATABASE_URL')
+DB_PATH = os.getenv('DATABASE_URL')
 
-class DatabaseContextManager:
+class DBContextManager:
     def __enter__(self):
-        self.conn = sqlite3.connect(DATABASE_PATH)
+        self.conn = sqlite3.connect(DB_PATH)
         return self.conn.cursor()
     
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.conn.commit()
         self.conn.close()
 
-def setup_database():
-    with DatabaseContextManager() as cursor:
+def initialize_database():
+    with DBContextManager() as cursor:
         cursor.execute('''CREATE TABLE IF NOT EXISTS workout_logs
                        (id INTEGER PRIMARY KEY, date TEXT, activity TEXT, duration INTEGER, intensity TEXT)''')
 
-class WorkoutLogger:
+class LogHandler:
     @staticmethod
-    def add_workout_log(date, activity, duration, intensity):
-        with DatabaseContextManager() as cursor:
+    def add_log(date, activity, duration, intensity):
+        with DBContextManager() as cursor:
             cursor.execute("INSERT INTO workout_logs (date, activity, duration, intensity) VALUES (?, ?, ?, ?)",
                         (date, activity, duration, intensity))
     
     @staticmethod
-    def fetch_all_logs():
-        with DatabaseContextManager() as cursor:
+    def get_all_logs():
+        with DBContextManager() as cursor:
             cursor.execute("SELECT * FROM workout_logs")
             return cursor.fetchall()
     
     @staticmethod
-    def modify_workout_log(log_id, date=None, activity=None, duration=None, intensity=None):
-        update_query = "UPDATE workout_logs SET "
-        parameters = []
+    def update_log(log_id, date=None, activity=None, duration=None, intensity=None):
+        base_query = "UPDATE workout_logs SET "
+        params = []
         if date:
-            update_query += "date = ?, "
-            parameters.append(date)
+            base_query += "date = ?, "
+            params.append(date)
         if activity:
-            update_query += "activity = ?, "
-            parameters.append(activity)
+            base_query += "activity = ?, "
+            params.append(activity)
         if duration:
-            update_query += "duration = ?, "
-            parameters.append(duration)
+            base_query += "duration = ?, "
+            params.append(duration)
         if intensity:
-            update_query += "intensity = ? "
-            parameters.append(intensity)
-        update_query = update_query.rstrip(", ")
-        update_query += " WHERE id = ?"
-        parameters.append(log_id)
-        with DatabaseContextManager() as cursor:
-            cursor.execute(update_query, tuple(parameters))
+            base_query += "intensity = ? "
+            params.append(intensity)
+        base_query = base_query.rstrip(", ")
+        base_query += " WHERE id = ?"
+        params.append(log_id)
+        with DBContextManager() as cursor:
+            cursor.execute(base_query, tuple(params))
     
     @staticmethod
-    def remove_workout_log(log_id):
-        with DatabaseContextManager() as cursor:
+    def delete_log(log_id):
+        with DBContextManager() as cursor:
             cursor.execute("DELETE FROM workout_logs WHERE id = ?", (log_id,))
 
-class WorkoutPredictor:
+class ActivityPredictor:
     @staticmethod
-    def display_workout_data_summary():
-        with DatabaseContextManager() as cursor:
-            dataframe = pd.read_sql_query("SELECT * FROM workout_logs", cursor.connection)
-            print(dataframe.describe())
+    def summarize_data():
+        with DBContextManager() as cursor:
+            df = pd.read_sql_query("SELECT * FROM workout_logs", cursor.connection)
+            print(df.describe())
     
     @staticmethod
-    def recommend_workout_plan(input_features):
-        with DatabaseContextManager() as cursor:
-            dataframe = pd.read_sql_query("SELECT * FROM workout_logs", cursor.connection)
-            if len(dataframe) < 10:  
-                return "Insufficient data for personalized workout recommendations."
+    def suggest_activity_plan(input_features):
+        with DBContextManager() as cursor:
+            df = pd.read_sql_query("SELECT * FROM workout_logs", cursor.connection)
+            if len(df) < 10:  
+                return "Insufficient data for personalized recommendations."
             
-            dataframe = pd.get_dummies(dataframe, columns=['intensity'])  
-            features = dataframe.drop(['id', 'date', 'activity'], axis=1)
-            target = dataframe['activity']
+            df = pd.get_dummies(df, columns=['intensity'])  
+            features = df.drop(['id', 'date', 'activity'], axis=1)
+            target = df['activity']
         
-            features_train, features_test, target_train, target_test = train_test_split(features, target, test_size=0.2, random_state=42)
+            X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=42)
         
-            classifier = RandomForestClassifier(n_estimators=100)
-            classifier.fit(features_train, target_train)
+            model = RandomForestClassifier(n_estimators=100)
+            model.fit(X_train, y_train)
         
-            recommended_activity = classifier.predict([input_features])
-            return recommended_activity
+            suggested_activity = model.predict([input_features])
+            return suggested_activity
 
     @staticmethod
-    def display_popular_workouts():
-        with DatabaseContextManager() as cursor:
+    def highlight_popular_activities():
+        with DBContextManager() as cursor:
             query = '''
             SELECT activity, intensity, COUNT(activity) as frequency 
             FROM workout_logs 
@@ -100,10 +100,10 @@ class WorkoutPredictor:
             LIMIT 3
             '''
             cursor.execute(query)
-            popular_workouts = cursor.fetchall()
-            print("Most Popular Workouts & Intensities:\n", popular_workouts)
+            popular_activities = cursor.fetchall()
+            print("Top Activities & Intensities:\n", popular_activities)
 
-def get_user_input():
+def collect_user_input():
     print("Enter workout log details")
     date = input("Date (YYYY-MM-DD): ")
     activity = input("Activity: ")
@@ -112,19 +112,19 @@ def get_user_input():
     return date, activity, duration, intensity
 
 if __name__ == "__main__":
-    setup_database()
+    initialize_database()
     while True:
-        user_choice = input("Choose an option: [1] Add Log [2] View Summary [3] View Popular Workouts [4] Exit: ")
-        if user_choice == "1":
-            date, activity, duration, intensity = get_user_input()
-            WorkoutLogger.add_workout_log(date, activity, duration, intensity)
-            print("Workout Log Added")
-        elif user_choice == "2":
-            WorkoutPredictor.display_workout_data_summary()
-        elif user_choice == "3":
-            WorkoutPredictor.display_popular_workouts()
-        elif user_choice == "4":
-            print("Exiting.")
+        choice = input("Choose an option: [1] Add Log [2] Data Summary [3] Popular Activities [4] Exit: ")
+        if choice == "1":
+            date, activity, duration, intensity = collect_user_input()
+            LogHandler.add_log(date, activity, duration, intensity)
+            print("Log Successfully Added")
+        elif choice == "2":
+            ActivityPredictor.summarize_data()
+        elif choice == "3":
+            ActivityPredictor.highlight_popular_activities()
+        elif choice == "4":
+            print("Goodbye.")
             break
         else:
             print("Invalid option, please try again.")
